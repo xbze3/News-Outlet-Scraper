10f4d646b5bc62607a34974464e4473566d5a717
Ezra Minty <155508437+xbze3@users.noreply.github.com>
2024-05-03 16:52:49 -0400

    Update web-scraper.py

--- web-scraper.py
+++ web-scraper.py
@@ -30,9 +30,31 @@ def getBloomberg(userInput):
     link = [a['href'] for a in soup.find_all('a', class_= lambda x: x and re.compile(r'^headline').match(x), href=True) if a.text]
     publishDate = [x.get_text() for x in soup.find_all('div', attrs={'class': lambda x: x and re.compile(r'^publishedAt').match(x)})]
 
+    print("\n " + "=" * 32 + " BLOOMBERG.COM " + "=" * 32)
+
     for i in range(0, len(eyebrowData)):
         print(f"\n [Sector]: {eyebrowData[i].upper()}\n [Headline]: {headlineData[i]}\n [Publish On]: {publishDate[i]}\n [Link]: {link[i]}")
 
-    print("\n " + "=" * 36 + " Done " + "=" * 36)
+def getInvesting(userInput):
+    
+    userInput = userInput.replace(" ", "%20")
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    URL = f"https://www.investing.com/search/?q={userInput}&tab=news"
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    headlineData = [x.get_text() for x in soup.find_all('a', attrs={'class': 'title'})]
+    link = [a['href'] for a in soup.find_all('a', class_= 'title', href=True) if a.text]
+    # publishDate = [x.get_text() for x in soup.find_all('time', attrs={'class': 'date'})]
+    # publishedBy = [x.find_all('span') for x in soup.find_all('div', class_='articleDetails')]
+
+    print("\n " + "=" * 32 + " INVESTING.COM " + "=" * 32)
+
+    for x in range(0, (len(headlineData) - 2)):
+        print(f"\n [Headline]: {headlineData[x]}\n [Link]: https://www.investing.com{link[x]}")
+
 
 getBloomberg(search)
+getInvesting(search)
+print("\n " + "=" * 36 + " Done " + "=" * 36)
